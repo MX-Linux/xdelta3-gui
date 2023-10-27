7e902216f3d02698565cdf7a6039511156ce0693
Adrian <adrian@mxlinux.org>
2023-10-27 09:19:07 -0400

    Small fix for translations

--- translations/xdelta3-gui_es_ES.ts
+++ translations/xdelta3-gui_es_ES.ts
@@ -1,237 +1,230 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="es_ES">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="24"/>
         <source>Create Patch</source>
         <translation>Crear parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="33"/>
         <source>Create patch</source>
-        <translation>Crear parche</translation>
+        <translation type="vanished">Crear parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="43"/>
         <source>Options</source>
         <translation>Opciones</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="86"/>
         <source>Compression level</source>
         <translation>Nivel de compresión</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
         <source>Secondary compression</source>
         <translation>Compresión secundaria</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="113"/>
-        <location filename="../mainwindow.ui" line="217"/>
         <source>File selection</source>
         <translation>Selección de archivos</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="125"/>
         <source>select target (new) file</source>
         <translation>seleccionar archivo de destino (nuevo)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="132"/>
         <source>patch.xdelta3</source>
         <translation>parche.xdelta3</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="139"/>
         <source>Target file</source>
         <translation>Archivo de destino</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="146"/>
         <source>Source file</source>
         <translation>Archivo fuente</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="153"/>
         <source>Patch name</source>
         <translation>Nombre del parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="160"/>
         <source>Select source file...</source>
         <translation>Seleccionar archivo fuente...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="173"/>
         <source>select source (old) file</source>
         <translation>seleccionar archivo fuente (antiguo)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="180"/>
         <source>Select target file...</source>
         <translation>Seleccionar archivo de destino...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="187"/>
         <source>Change location...</source>
         <translation>Cambiar ubicación...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="211"/>
-        <location filename="../mainwindow.ui" line="308"/>
         <source>Apply Patch</source>
         <translation>Aplicar parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="223"/>
         <source>Input file</source>
         <translation>Archivo de entrada</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="230"/>
         <source>select input file</source>
         <translation>seleccionar archivo de entrada</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="237"/>
         <source>Select input file...</source>
         <translation>Seleccionar archivo de entrada...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="244"/>
         <source>Patch file</source>
         <translation>Archivo parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="251"/>
         <source>select patch</source>
         <translation>seleccionar parche</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="258"/>
         <source>Select patch...</source>
         <translation>Seleccionar parche...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="265"/>
         <source>Output file</source>
         <translation>Archivo de salida</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="275"/>
         <source>use default, or set up a custom name</source>
         <translation>utilice el nombre predeterminado o configure un nombre personalizado</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="282"/>
         <source>Change location..</source>
         <translation>Cambiar ubicación...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="91"/>
         <source>ISO Files (*.iso);;All Files (*)</source>
         <translation>Archivos ISO (*.iso);;Todos los archivos (*)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="92"/>
         <source>ISO Files (*.xdelta3);;All Files (*)</source>
         <translation>Archivos ISO (*.xdelta3);;Todos los archivos (*)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="93"/>
         <source>Select file</source>
         <translation>Seleccionar archivo</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="118"/>
         <source>Path not found</source>
         <translation>Ruta no encontrada</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="118"/>
         <source>Please select directory again.</source>
         <translation>Por favor, seleccione de nuevo el directorio.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="127"/>
-        <location filename="../mainwindow.cpp" line="145"/>
-        <location filename="../mainwindow.cpp" line="156"/>
-        <location filename="../mainwindow.cpp" line="195"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="127"/>
         <source>Please enter a name for the output file.</source>
         <translation>Por favor, introduzca un nombre para el archivo de salida.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="135"/>
-        <location filename="../mainwindow.cpp" line="182"/>
         <source>File exists</source>
         <translation>El archivo ya existe</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="135"/>
         <source>Output file exists, do you want to overwrite?</source>
         <translation>El archivo de salida existe, ¿desea sobrescribirlo?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="143"/>
-        <location filename="../mainwindow.cpp" line="193"/>
         <source>Success</source>
         <translation>Exito</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="143"/>
-        <location filename="../mainwindow.cpp" line="193"/>
         <source>File was successfuly written.</source>
-        <translation>El archivo se ha escrito correctamente.</translation>
+        <translation type="vanished">El archivo se ha escrito correctamente.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="145"/>
-        <location filename="../mainwindow.cpp" line="195"/>
         <source>Error: Could not finish writing the file.</source>
-        <translation>Error: No se pudo terminar de escribir el archivo.</translation>
+        <translation type="vanished">Error: No se pudo terminar de escribir el archivo.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="156"/>
         <source>Please enter a name for the patch file.</source>
         <translation>Por favor, introduzca un nombre para el archivo de parche.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="169"/>
         <source>File not found</source>
         <translation>Archivo no encontrado</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="170"/>
         <source>File &apos;%1&apos; found, please double-check the input.</source>
-        <translation>Archivo &apos;%1&apos; encontrado, por favor, compruebe la entrada.</translation>
+        <translation type="vanished">Archivo &apos;%1&apos; encontrado, por favor, compruebe la entrada.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="182"/>
         <source>Patch file exists, do you want to overwrite?</source>
         <translation>El archivo de parche existe, ¿desea sobrescribirlo?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="236"/>
         <source>Cancel</source>
         <translation>Cancelar</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="242"/>
         <source>Please wait...</source>
-        <translation>Por favor, espere...</translation>
+        <translation type="vanished">Por favor, espere...</translation>
+    </message>
+    <message>
+        <source>File was successfuly written to &apos;%1&apos; directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Took %1 to patch the file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Could not write the file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; found or not a file, please double-check the input.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; was successfuly written.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Took %1 to create the patch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 elapsed</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="66"/>
         <source>You must run this program as normal user.</source>
         <translation>Debe ejecutar este programa como usuario normal.</translation>
+    </message>
+    <message>
+        <source>GUI for xdelta3 application.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name of the patch file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>[file]</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
