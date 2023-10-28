fa9ee8f3bcdd4974d61dbc463aedd840001244a2
Dolphin Oracle <dolphinoracle@gmail.com>
2023-10-27 20:01:32 -0400

    add translator comments

--- translations/xdelta3-gui_am.ts
+++ translations/xdelta3-gui_am.ts
@@ -5,6 +5,7 @@
     <name>MainWindow</name>
     <message>
         <source>Options</source>
+        <extracomment>program options</extracomment>
         <translation>ምርጫዎች</translation>
     </message>
     <message>
@@ -12,35 +13,23 @@
         <translation>ስህተት</translation>
     </message>
     <message>
-        <source>Success</source>
-        <translation>ተሳክቷል</translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation>መሰረዣ</translation>
-    </message>
-    <message>
-        <source>Please wait...</source>
-        <translation type="vanished">እባክዎን ይቆዩ...</translation>
-    </message>
-    <message>
         <source>Compression level</source>
+        <extracomment>level of compression the delta file will use</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Secondary compression</source>
+        <extracomment>an extra layer of compression</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>File selection</source>
+        <extracomment>area for selecting input and output files</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>select target (new) file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>patch.xdelta3</source>
+        <extracomment>select the new file from which to create a delta between it and the source.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -53,62 +42,52 @@
     </message>
     <message>
         <source>Select source file...</source>
+        <extracomment>starts a file chooser to select a file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>select source (old) file</source>
+        <extracomment>select the original or source file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Select target file...</source>
+        <extracomment>starts a file chooser to select a file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Change location...</source>
+        <extracomment>starts a file chooser to select destination for output delta file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Input file</source>
+        <extracomment>a file to start with as the source or input</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>select input file</source>
+        <extracomment>select the input or older file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Select input file...</source>
+        <extracomment>starts a file chooser to select a file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Output file</source>
+        <extracomment>the final patched output file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>use default, or set up a custom name</source>
+        <extracomment>create the new file name</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Change location..</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>ISO Files (*.iso);;All Files (*)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>ISO Files (*.xdelta3);;All Files (*)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Select file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Path not found</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Please select directory again.</source>
+        <extracomment>starts a file chooser to select destination for output file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -120,47 +99,15 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Output file exists, do you want to overwrite?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File was successfuly written to &apos;%1&apos; directory.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Took %1 to patch the file.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Error: Could not write the file.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File not found</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File &apos;%1&apos; found or not a file, please double-check the input.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File &apos;%1&apos; was successfuly written.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Took %1 to create the patch.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>%1 elapsed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Create delta</source>
+        <extracomment>meaning to create a delta file that bridges between the source and target file
+----------
+start process</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Create a binary difference between two files</source>
+        <extracomment>meaning to create a delta file that bridges between the source and target file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -173,43 +120,152 @@
     </message>
     <message>
         <source>Apply delta</source>
+        <extracomment>meaning to apply a delta file to a input file to create a new output file
+----------
+start the process</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Create a new file applying a delta (difference) file to an original file</source>
+        <extracomment>meaning to apply a delta file to a input file to create a new output file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Delta file</source>
+        <extracomment>the delta or diff file between the input and the output</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>select delta</source>
+        <extracomment>select the delta or patch file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Select delta...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Please enter a name for the delta file.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Delta file exists, do you want to overwrite?</source>
+        <extracomment>starts a file chooser to select a file</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Higher number = better compression ratio, but takes more time to finish</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ISO Files (*.iso);;All Files (*)</source>
+        <comment>kinds of files to choose</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ISO Files (*.xdelta3);;All Files (*)</source>
+        <comment>kinds of files to choose</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select file</source>
+        <comment>choose a file</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select directory to place the file in</source>
+        <comment>select a target directory</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path not found</source>
+        <comment>warning about not finding the target directory</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please select directory again.</source>
+        <comment>message to try again</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Output file exists, do you want to overwrite?</source>
+        <comment>warning about overwritting an existing file</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <comment>information that file was successfully written</comment>
+        <translation type="unfinished">ተሳክቷል</translation>
+    </message>
+    <message>
+        <source>File was successfuly written to &apos;%1&apos; directory.</source>
+        <comment>information that file was successfully written</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Took %1 to patch the file.</source>
+        <comment>elapsed time, leave %1 untranslated</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Could not write the file.</source>
+        <comment>information that there was an error creating the file</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter a name for the delta file.</source>
+        <comment>name of delta file being created</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File not found</source>
+        <comment>warning about file not found</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; found or not a file, please double-check the input.</source>
+        <comment>warning about file not found</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File exists</source>
+        <comment>warning about overwritting file</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delta file exists, do you want to overwrite?</source>
+        <comment>warning about overwritting file</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <comment>information on file written succesfully</comment>
+        <translation type="unfinished">ተሳክቷል</translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; was successfuly written.</source>
+        <comment>information on file written succesfully</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Took %1 to create the patch.</source>
+        <comment>elasped time, leave %1 untranslated</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Could not write the file.</source>
+        <comment>information that file was not written succsessfully</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <comment>stop an action in progress</comment>
+        <translation type="unfinished">መሰረዣ</translation>
+    </message>
+    <message>
+        <source>%1 elapsed</source>
+        <comment>elasped time, leave %1 untranslated</comment>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QObject</name>
-    <message>
-        <source>You must run this program as normal user.</source>
-        <translation>እርስዎ ይህን ፕሮግራም እንደ መደበኛ ተጠቃሚ ማስኬድ አለብዎት</translation>
-    </message>
     <message>
         <source>file</source>
         <translation type="unfinished"></translation>
@@ -219,12 +275,23 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>GUI for xdelta3 application. App for generating a binary difference between two files in the form of delta encoding</source>
+        <source>Name of the delta file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Name of the delta file</source>
+        <source>GUI for xdelta3 application.</source>
+        <comment>short description for --help and desktop file</comment>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>App for generating a binary difference between two files in the form of delta encoding</source>
+        <comment>extra description for --help</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You must run this program as normal user.</source>
+        <comment>warning about running application as root</comment>
+        <translation type="unfinished">እርስዎ ይህን ፕሮግራም እንደ መደበኛ ተጠቃሚ ማስኬድ አለብዎት</translation>
     </message>
 </context>
 </TS>
