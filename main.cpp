4239e99ac66125bbe11dbe07168fb4c91029c8ab
Adrian <adrian@mxlinux.org>
2025-08-24 23:05:31 -0400

    Simplify version handling

--- main.cpp
+++ main.cpp
@@ -29,9 +29,12 @@
 #include <QTranslator>
 
 #include "mainwindow.h"
-#include "version.h"
 
 #include <unistd.h>
+
+#ifndef VERSION
+    #define VERSION "?.?.?.?"
+#endif
 
 int main(int argc, char *argv[])
 {
